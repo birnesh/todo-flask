beb40026fd9e7ca5e85a74edd5a6cf1021222826
birnesh <birnesh1996@gmail.com>
2020-05-31 02:22:48 +0900

    updated the error message in the decorator

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def login_required(f):
             data = jwt.decode(token, app.config['SECRET_KEY'])
             current_user = User.query.filter_by(public_id=data['public_id']).first()
         except:
-            return jsonify({'message':'Token is missing'}), 401
+            return jsonify({'message':'Token is invalid!'}), 401
         return f(current_user,*args, **kwargs)
     return decorated
 
